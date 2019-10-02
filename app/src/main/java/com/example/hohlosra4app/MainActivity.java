d546fd6fafea5997c2adbfe22c9683b8735adf7e
anatolf <gertz.promo@gmail.com>
2019-10-02 13:48:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hohlosra4app/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.hohlosra4app;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> tvChannelArrayList = new ArrayList<>();
+    ArrayList<Integer> usersIntoChatArrayList = new ArrayList<>();
+
+    private RecyclerView rvTvChannelsList;
+    private RecyclerView.LayoutManager rvLayoutManager;
+    private RecyclerViewAdapter tvChannelsAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initRecyclerView();
+        loadChannels();
+    }
+
+    private void initRecyclerView(){
+
+        // Attach RecyclerView xml item layout
+        rvTvChannelsList = (RecyclerView) findViewById(R.id.rv_tv_channels);
+
+
+        // User this to display items vertically
+        //rvLayoutManager = new LinearLayoutManager(this);
+
+        // User this to display items in Grid Layout with 2 columns
+        rvLayoutManager = new GridLayoutManager(this, 2);
+
+        // Attach layoutManager to recycler view
+        rvTvChannelsList.setLayoutManager(rvLayoutManager);
+
+        // Pass data to our custom adapter
+        tvChannelsAdapter = new RecyclerViewAdapter(this, tvChannelArrayList, usersIntoChatArrayList);
+
+        // Attach custom adapter to recycler view
+        rvTvChannelsList.setAdapter(tvChannelsAdapter);
+
+
+////////////////experiment/////////////////////////////
+// при нажатии на элемент получаем его порядковый номер в Тоаст
+        RecyclerViewAdapter.OnChannelClickListner onChannelClickListner = new RecyclerViewAdapter.OnChannelClickListner() {
+            @Override
+            public void onChannelClick(Integer channel) {
+                Toast.makeText(MainActivity.this,
+                            "Into MainActivity: " + channel,
+                            Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        tvChannelsAdapter = new RecyclerViewAdapter(onChannelClickListner);
+        rvTvChannelsList.setAdapter(tvChannelsAdapter);
+
+//end/////////////////////////////////////////////////
+    }
+
+    private void loadChannels(){
+        // набиваем Демо лист значениями:
+        // Каналы:
+        tvChannelArrayList.add("TvChannel 1");
+        tvChannelArrayList.add("TvChannel 2");
+        tvChannelArrayList.add("TvChannel 3");
+        tvChannelArrayList.add("TvChannel 4");
+        tvChannelArrayList.add("TvChannel 5");
+        tvChannelArrayList.add("TvChannel 6");
+        tvChannelArrayList.add("TvChannel 7");
+        tvChannelArrayList.add("TvChannel 8");
+        tvChannelArrayList.add("TvChannel 9");
+        tvChannelArrayList.add("TvChannel 10");
+
+        // Юзеры в чате:
+        usersIntoChatArrayList.add(45);
+        usersIntoChatArrayList.add(0);
+        usersIntoChatArrayList.add(0);
+        usersIntoChatArrayList.add(6);
+        usersIntoChatArrayList.add(123);
+        usersIntoChatArrayList.add(0);
+        usersIntoChatArrayList.add(0);
+        usersIntoChatArrayList.add(15);
+        usersIntoChatArrayList.add(0);
+        usersIntoChatArrayList.add(0);
+
+        tvChannelsAdapter.setChannelAndUsersIntoList(tvChannelArrayList,usersIntoChatArrayList);
+    }
+}
---
+++ app/src/main/java/com/example/hohlosra4app/RecyclerViewAdapter.java
@@ -0,0 +1,130 @@
+package com.example.hohlosra4app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    // Create two empty arrayList and one context variable;
+    Context mainActivityContext;
+    ArrayList<String> channelsArrayList = new ArrayList<>();
+    ArrayList<Integer> usersIntoChatArrayList = new ArrayList<>();
+
+    private OnChannelClickListner onChannelClickListner;  // для передачи данных в MainActivity
+
+//// конструктор для передачи данных из RecyclerView в MainActivity:
+    public RecyclerViewAdapter(OnChannelClickListner onChannelClickListner) {
+        this.onChannelClickListner = onChannelClickListner;
+    }
+
+//// Create one constructor with three parameters which will passed from MainActivity class
+    public RecyclerViewAdapter(Context mainActivityContext, ArrayList<String> channelsArrayList, ArrayList<Integer> countUsersArrayList) {
+        this.mainActivityContext = mainActivityContext;
+      //  this.channelsArrayList = channelsArrayList;
+      //  this.usersIntoChatArrayList = countUsersArrayList;
+    }
+
+//// Устанавливает список Каналов и список активных Юзеров в канале:
+    public void setChannelAndUsersIntoList(ArrayList<String> channels, ArrayList<Integer> usersInto){
+        channelsArrayList.addAll(channels);
+        usersIntoChatArrayList.addAll(usersInto);
+        notifyDataSetChanged();
+    }
+
+//// Очищает список Каналов и список активных Юзеров в канале:
+    public void clearChannelAndUsersIntoList(){
+        channelsArrayList.clear();
+        usersIntoChatArrayList.clear();
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Used to connect our custom UI to our recycler view // надувает представление одного элемента
+
+        View v = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.recyclerview_item, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+
+    @Override
+    public void onBindViewHolder(RecyclerViewAdapter.ViewHolder holder, int position) {
+        //Used to set data in each row of recycler view // обновляет при прокрутке
+
+        String currentTvChannel = channelsArrayList.get(position);
+        String countUsers = usersIntoChatArrayList.get(position).toString();
+
+        holder.tvChannelName.setText(currentTvChannel);
+        holder.tvUsersIntoChat.setText(countUsers);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        //Returns total number of rows inside recycler view  // возвращает количество элементов списка
+        return channelsArrayList.size();
+    }
+
+
+////////////// experiment ////////////////////
+    public interface OnChannelClickListner{  // для передачи данных в майн создаем слушатель
+        void onChannelClick(Integer channel);
+    }
+//end////////////////////////////////////////
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        //Used to work with the elements of our custom UI.
+
+        LinearLayout llItemTvChannels;
+        TextView tvChannelName;
+        TextView tvUsersIntoChat;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            llItemTvChannels = itemView.findViewById(R.id.llItemTvChannels);
+            tvChannelName = itemView.findViewById(R.id.tv_channel_name);
+            tvUsersIntoChat =  itemView.findViewById(R.id.tvUsersIntoChat);
+
+////////////experiment////////////
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   int channelNumber = getAdapterPosition();  // передаём в MainActivity индекс элемента по которому нажали
+                    onChannelClickListner.onChannelClick(channelNumber);
+                }
+            });
+//end//////////////////////////////
+//            llItemTvChannels.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+////                    Toast.makeText(mainActivityContext,
+////                            "You clicked item number: " + getAdapterPosition(),
+////                            Toast.LENGTH_SHORT).show();
+//
+//                  //  Intent chatIntent = new Intent(mainActivityContext, ChatActivity.class);
+//
+//                  //  startActivity(chatIntent);
+//
+//                }
+//            });
+
+        }
+    }
+}
