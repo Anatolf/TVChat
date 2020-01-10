19413b26004ada13b2ebe09fa0b691736ff73778
anatolf <gertz.promo@gmail.com>
2020-01-11 00:45:15 +0300

    Refactoring

--- app/src/test/java/com/anatolf/tvchat/utils/ExampleUnitTest.java
+++ app/src/test/java/com/anatolf/tvchat/utils/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.anatolf.tvchat;
+package com.anatolf.tvchat.utils;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
